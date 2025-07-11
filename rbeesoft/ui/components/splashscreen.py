1293ce8a1f8b08c57535239bf9b96efb50a58045
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-07-11 14:01:54 +0200

    Saving work

---
+++ rbeesoft/ui/components/splashscreen.py
@@ -0,0 +1,121 @@
+import os
+import webbrowser
+
+from PySide6.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QPushButton,
+    QLabel,
+)
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap
+
+from rbeesoft.ui.constants import Constants
+
+from rbeesoft.ui.utils import resource_path, set_opacity
+
+
+class SplashScreen(QWidget):
+    def __init__(self, main_window, title, background_image_file_path=None):
+        super(SplashScreen, self).__init__()
+        self._main_window = main_window
+        self._title = title
+        self._background_image_file_path = background_image_file_path
+        self._background_label = None
+        self._background_pixmap = None
+        self._title_label = None
+        self._sub_text_label = None
+        self._start_app_button = None
+        self._donate_button = None
+        self._close_button = None
+        self.init_layout()
+
+    def main_window(self):
+        return self._main_window
+    
+    def title(self):
+        return self._title
+    
+    def background_image_file_path(self):
+        return self._background_image_file_path
+    
+    def background_label(self):
+        if not self._background_label and self._background_image_file_path:
+            self._background_label = QLabel(self)
+            self._background_label.setPixmap(self.background_pixmap())
+            self._background_label.setGeometry(0, 0, self.width(), self.height())
+            self._background_label.lower()
+        return self._background_label
+    
+    def background_pixmap(self):
+        if not self._background_pixmap and self._background_image_file_path:
+            self._background_pixmap = QPixmap(resource_path(os.path.join(
+                Constants.RBEESOFT_RESOURCES_IMAGES_DIR, 
+                Constants.RBEESOFT_SPLASH_SCREEN_BACKGROUND_IMAGE,
+            ))).scaled(self.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+            self._background_pixmap = set_opacity(self._background_pixmap, Constants.RBEESOFT_SPLASH_SCREEN_BACKGROUND_IMAGE_OPACITY)
+        return self._background_pixmap
+    
+    def title_label(self):
+        if not self._title_label:
+            self._title_label = QLabel(Constants.RBEESOFT_SPLASH_SCREEN_TITLE)
+            self._title_label.setStyleSheet(Constants.RBEESOFT_SPLASH_SCREEN_TITLE_STYLE_SHEET)
+            self._title_label.setAlignment(Qt.AlignCenter)
+        return self._title_label
+    
+    def sub_text_label(self):
+        if not self._sub_text_label:
+            self._sub_text_label = QLabel(Constants.RBEESOFT_SPLASH_SCREEN_SUB_TEXT)
+            self._sub_text_label.setStyleSheet(Constants.RBEESOFT_SPLASH_SCREEN_SUB_TEXT_STYLE_SHEET)
+            self._sub_text_label.setAlignment(Qt.AlignCenter)
+        return self._sub_text_label
+    
+    def start_app_button(self):
+        if not self._start_app_button:
+            self._start_app_button = QPushButton(Constants.RBEESOFT_SPLASH_SCREEN_START_APP_BUTTON_TEXT)
+            self._start_app_button.clicked.connect(self.handle_start_app_button)
+        return self._start_app_button
+    
+    def donate_button(self):
+        if not self._donate_button:
+            self._donate_button = QPushButton(Constants.RBEESOFT_DONATE_BUTTON_TEXT)
+            self._donate_button.setStyleSheet(Constants.RBEESOFT_DONATE_BUTTON_STYLESHEET)
+            self._donate_button.clicked.connect(self.handle_donate_button)
+        return self._donate_button
+    
+    def close_button(self):
+        if not self._close_button:
+            self._close_button = QPushButton(Constants.RBEESOFT_SPLASH_SCREEN_CLOSE_BUTTON_TEXT)
+            self._close_button.clicked.connect(self.handle_close_button)
+        return self._close_button
+    
+    # LAYOUT
+
+    def init_layout(self):
+        layout = QVBoxLayout()
+        layout.addWidget(self.title_label())
+        layout.addWidget(self.sub_text_label())
+        layout.addWidget(self.start_app_button())
+        # layout.addWidget(self.donate_button())
+        layout.addWidget(self.close_button())
+        self.setLayout(layout)
+        self.setFixedSize(Constants.RBEESOFT_SPLASH_SCREEN_WINDOW_W, Constants.RBEESOFT_SPLASH_SCREEN_WINDOW_H)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+    
+    # EVENT HANDLERS
+
+    def handle_start_app_button(self):
+        self.close()
+        self.main_window().show()
+
+    def handle_donate_button(self):
+        webbrowser.open(Constants.RBEESOFT_DONATE_URL)
+
+    def handle_close_button(self):
+        self.close()
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        scaled = self.background_pixmap().scaled(self.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
+        self.background_label().setPixmap(scaled)
+        self.background_label().setGeometry(0, 0, self.width(), self.height())
--- rbeesoft/ui/mainwindow.py
+++ rbeesoft/ui/mainwindow.py
@@ -5,59 +5,27 @@ from PySide6.QtWidgets import (
 )
 from PySide6.QtGui import (
     QGuiApplication,
-    QAction,
     QIcon,
 )
 from PySide6.QtCore import Qt, QByteArray
 
-import rbeesoft.ui.constants as constants
-
+from rbeesoft.ui.constants import Constants
 from rbeesoft.ui.settings import Settings
-from rbeesoft.ui.panels.mainpanel import MainPanel
-from rbeesoft.ui.panels.logpanel import LogPanel
-from rbeesoft.ui.panels.settingspanel import SettingsPanel
-from rbeesoft.ui.utils import resource_path, version, is_macos
-from rbeesoft.core.utils.logmanager import LogManager
-
-LOG = LogManager()
+from rbeesoft.ui.utils import resource_path, version
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self._settings = None
-        self._main_panel = None
-        self._settings_panel = None
-        self._log_panel = None
         self.init_window()
 
     def init_window(self):
-        self.setWindowTitle(f'{constants.RBEESOFT_WINDOW_TITLE} {version(constants.RBEESOFT_NAME)}')
+        self.setWindowTitle(f'{Constants.RBEESOFT_WINDOW_TITLE} {version(Constants.RBEESOFT_NAME)}')
         self.setWindowIcon(QIcon(resource_path(os.path.join(
-            constants.RBEESOFT_RESOURCES_IMAGES_ICONS_DIR, constants.RBEESOFT_RESOURCES_ICON))))
+            Constants.RBEESOFT_RESOURCES_IMAGES_ICONS_DIR, Constants.RBEESOFT_RESOURCES_ICON))))
         if not self.load_geometry_and_state():
             self.set_default_size_and_position()
-        self.init_menus()
-        self.init_status_bar()
-        self.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, self.main_panel())
-        self.addDockWidget(Qt.DockWidgetArea.BottomDockWidgetArea, self.log_panel())
-
-    def init_menus(self):
-        self.init_app_menu()
-        if is_macos():            
-            self.menuBar().setNativeMenuBar(False)
-
-    def init_app_menu(self):
-        open_settings_action = QAction(constants.RBEESOFT_APP_MENU_OPEN_SETTINGS_PANEL_ACTION_TEXT, self)
-        open_settings_action.triggered.connect(self.handle_open_settings_panel_action)
-        exit_action = QAction(constants.RBEESOFT_APP_MENU_EXIT_ACTION_TEXT, self)
-        exit_action.triggered.connect(self.close)
-        app_menu = self.menuBar().addMenu(constants.RBEESOFT_APP_MENU_TEXT)
-        app_menu.addAction(open_settings_action)
-        app_menu.addAction(exit_action)
-
-    def init_status_bar(self):
-        self.set_status(constants.RBEESOFT_STATUS_READY)
 
     # GETTERS
 
@@ -66,35 +34,6 @@ class MainWindow(QMainWindow):
             self._settings = Settings()
         return self._settings
     
-    def main_panel(self):
-        if not self._main_panel:
-            self._main_panel = MainPanel(self)
-            self._main_panel.add_panel(
-                self.settings_panel(), constants.RBEESOFT_SETTINGS_PANEL_NAME)
-            self._main_panel.select_panel(constants.RBEESOFT_SETTINGS_PANEL_NAME)
-        return self._main_panel
-    
-    def settings_panel(self):
-        if not self._settings_panel:
-            self._settings_panel = SettingsPanel()
-        return self._settings_panel
-    
-    def log_panel(self):
-        if not self._log_panel:
-            self._log_panel = LogPanel()
-            LOG.add_listener(self._log_panel)
-        return self._log_panel
-    
-    # SETTERS
-
-    def set_status(self, message):
-        self.statusBar().showMessage(message)
-
-    # EVENT HANDLERS
-
-    def handle_open_settings_panel_action(self):
-        self.main_panel().select_panel(constants.RBEESOFT_SETTINGS_PANEL_NAME)
-
     def closeEvent(self, event):
         self.save_geometry_and_state()
         return super().closeEvent(event)
@@ -102,8 +41,8 @@ class MainWindow(QMainWindow):
     # MISCELLANEOUS
 
     def load_geometry_and_state(self):
-        geometry = self.settings().get(constants.RBEESOFT_WINDOW_GEOMETRY_KEY)
-        state = self.settings().get(constants.RBEESOFT_WINDOW_STATE_KEY)
+        geometry = self.settings().get(Constants.RBEESOFT_WINDOW_GEOMETRY_KEY)
+        state = self.settings().get(Constants.RBEESOFT_WINDOW_STATE_KEY)
         if isinstance(geometry, QByteArray) and self.restoreGeometry(geometry):
             if isinstance(state, QByteArray):
                 self.restoreState(state)
@@ -117,7 +56,7 @@ class MainWindow(QMainWindow):
             constants.RBEESOFT_WINDOW_STATE_KEY, self.saveState())
 
     def set_default_size_and_position(self):
-        self.resize(constants.RBEESOFT_WINDOW_W, constants.RBEESOFT_WINDOW_H)
+        self.resize(Constants.RBEESOFT_WINDOW_W, Constants.RBEESOFT_WINDOW_H)
         self.center_window()
 
     def center_window(self):
